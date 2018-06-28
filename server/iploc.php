85be31f66f6d4def1194af5791c1d020421fb0cd
frenchincubus <lionelrenier@sfr.fr>
2018-06-28 18:01:15 +0200

    version 5 modifying API loc access

--- server/iploc.php
+++ server/iploc.php
@@ -1,6 +1,7 @@
 <?php
-$ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
+//$ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
 // $ip = get_ip();
+$ip = get_ip_address();
 function get_country() {
     $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip)); //connection au serveur de ip-api.com et recuperation des données
 if($query && $query['status'] == 'success') 
@@ -32,4 +33,16 @@ function get_ip() {
 		return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
     }
 }
-?>
+
+function get_ip_address() {
+	if ( isset( $_SERVER['HTTP_X_REAL_IP'] ) ) {
+		return $_SERVER['HTTP_X_REAL_IP'];
+	} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+		// Proxy servers can send through this header like this: X-Forwarded-For: client1, proxy1, proxy2
+		// Make sure we always only send through the first IP in the list which should always be the client IP.
+		return (string) is_ip_address( trim( current( explode( ',', $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) ) );
+	} elseif ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
+		return $_SERVER['REMOTE_ADDR'];
+	}
+	return '';
+}
