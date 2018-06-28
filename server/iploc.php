1bfb2105221f101a807f27b3b75da10c6da8b803
frenchincubus <lionelrenier@sfr.fr>
2018-06-28 18:09:17 +0200

    version 5 modifying API loc access

--- server/iploc.php
+++ server/iploc.php
@@ -40,7 +40,7 @@ function get_ip_address() {
 	} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
 		// Proxy servers can send through this header like this: X-Forwarded-For: client1, proxy1, proxy2
 		// Make sure we always only send through the first IP in the list which should always be the client IP.
-		return (string) is_ip_address( trim( current( explode( ',', $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) ) );
+		return $_SERVER['HTTP_X_FORWARDED_FOR'];
 	} elseif ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
 		return $_SERVER['REMOTE_ADDR'];
 	}
