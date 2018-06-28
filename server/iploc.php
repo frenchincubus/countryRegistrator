4f1dccbfa48bc6cbaae2228a9db63f38f393cd1c
frenchincubus <lionelrenier@sfr.fr>
2018-06-28 18:44:21 +0200

    version 5 modifying API loc access

--- server/iploc.php
+++ server/iploc.php
@@ -1,7 +1,7 @@
 <?php
-//$ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
+$ip = $_REQUEST['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
 // $ip = get_ip();
-$ip = get_ip_address();
+//$ip = get_ip_address();
 function get_country() {
     $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip)); //connection au serveur de ip-api.com et recuperation des données
 if($query && $query['status'] == 'success') 
@@ -19,30 +19,30 @@ if($query && $query['status'] == 'success')
 }
 
 
-function get_ip() {
-	// IP si internet partagé
-	if (isset($_SERVER['HTTP_CLIENT_IP'])) {
-		return $_SERVER['HTTP_CLIENT_IP'];
-	}
-	// IP derrière un proxy
-	elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-		return $_SERVER['HTTP_X_FORWARDED_FOR'];
-	}
-	// Sinon : IP normale
-	else {
-		return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
-    }
-}
+// function get_ip() {
+// 	// IP si internet partagé
+// 	if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+// 		return $_SERVER['HTTP_CLIENT_IP'];
+// 	}
+// 	// IP derrière un proxy
+// 	elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+// 		return $_SERVER['HTTP_X_FORWARDED_FOR'];
+// 	}
+// 	// Sinon : IP normale
+// 	else {
+// 		return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
+//     }
+// }
 
-function get_ip_address() {
-	if ( isset( $_SERVER['HTTP_X_REAL_IP'] ) ) {
-		return $_SERVER['HTTP_X_REAL_IP'];
-	} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-		// Proxy servers can send through this header like this: X-Forwarded-For: client1, proxy1, proxy2
-		// Make sure we always only send through the first IP in the list which should always be the client IP.
-		return $_SERVER['HTTP_X_FORWARDED_FOR'];
-	} elseif ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
-		return $_SERVER['REMOTE_ADDR'];
-	}
-	return '';
-}
+// function get_ip_address() {
+// 	if ( isset( $_SERVER['HTTP_X_REAL_IP'] ) ) {
+// 		return $_SERVER['HTTP_X_REAL_IP'];
+// 	} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+// 		// Proxy servers can send through this header like this: X-Forwarded-For: client1, proxy1, proxy2
+// 		// Make sure we always only send through the first IP in the list which should always be the client IP.
+// 		return $_SERVER['HTTP_X_FORWARDED_FOR'];
+// 	} elseif ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
+// 		return $_SERVER['REMOTE_ADDR'];
+// 	}
+// 	return '';
+// }
